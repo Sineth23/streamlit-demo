0a3cc4b7fcc23f3f0aceef8d47864a630cd490aa
Sineth23 <57430821+Sineth23@users.noreply.github.com>
2025-01-29 11:45:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+#os.environ["ACTIVELOOP_TOKEN"] = st.secrets["ACTIVELOOP_TOKEN"]
+#dataset_path = st.sidebar.text_input("Dataset Path:", value="hub://autodoctest/smiddygroup-waio-portal-backend")
+
 import streamlit as st
 import pandas as pd
 import os
 from SRED import QueryCodebase  # Ensure the import matches your local module structure
 from langchain_community.vectorstores import DeepLake
+
 os.environ["ACTIVELOOP_TOKEN"] = st.secrets["ACTIVELOOP_TOKEN"]
 # Streamlit Page Configuration
 st.set_page_config(
@@ -14,6 +18,7 @@ st.set_page_config(
 # Sidebar Configuration
 st.sidebar.title("SR&ED Chatbot Configuration")
 dataset_path = st.sidebar.text_input("Dataset Path:", value="hub://autodoctest/smiddygroup-waio-portal-backend")
+
 model_name = st.sidebar.text_input("Model Name:", value="sentence-transformers/all-MiniLM-L6-v2")
 openai_key = st.sidebar.text_input("OpenAI API Key:", value=os.getenv('OPENAI_API_KEY'), type="password")
 
@@ -84,7 +89,7 @@ if st.button("Run Query"):
         try:
             with st.spinner("Processing your query..."):
                 # Perform the query with the combined text
-                answer = query_codebase.perform_query(full_query)
+                answer = query_codebase.perform_query(full_query, 'General')
             
             # Display the Answer
             st.subheader("Answer:")
@@ -93,4 +98,3 @@ if st.button("Run Query"):
             st.error(f"An error occurred: {e}")
     else:
         st.warning("Please select an issue key and enter a query.")
-
